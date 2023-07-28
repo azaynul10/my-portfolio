d65c8adc78e47f73341dec8abe8535a1452fe4c4
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2023-07-28 18:27:03 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Zaynul Abedin Miah",
+  title: "Undergraduate Computer Science Student",
+  email: "Zaynul.AbedinMiah@studentambassadors.com",
+  gitHub: "azaynul10",
+  instagram: "__zayn_abedin_miah/",
+  linkedIn: "zaynul-abedin-miah",
+  dev: "https://dev.to/azaynul10/",
+  twitter: "azaynul123",
+  youTube: "zaynulabedin",
 };
 
 const primaryColor = "#4E567E";
